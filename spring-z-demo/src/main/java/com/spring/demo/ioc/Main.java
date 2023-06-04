177bd5ce306aaacd432bf2fbcaaefd9c29f35bb5
luke <gitluke@163.com>
2023-06-04 14:14:15 +0800

    edit

--- spring-z-demo/src/main/java/com/spring/demo/ioc/Main.java
+++ spring-z-demo/src/main/java/com/spring/demo/ioc/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(IOCConfig.class);
 		IOCBean iocBean = context.getBean(IOCBean.class);
+		AutowiredBean autowiredBean = context.getBean(AutowiredBean.class);
 		iocBean.print();
+		autowiredBean.print();
 	}
 }
